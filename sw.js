8cc069c40969f331343320fa48ae1804a02d598a
damianodionori <d.dionori@gmail.com>
2025-04-27 20:58:07 +0100

    pwa

---
+++ sw.js
@@ -0,0 +1,52 @@
+const CACHE_NAME = 'ds-tours-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/about-us.html',
+    '/tours.html',
+    '/contact.html',
+    '/styles.css',
+    '/script.js',
+    '/components.js',
+    '/config.js',
+    '/manifest.json',
+    '/images/logo-ds-tours.png',
+    '/images/icons/icon-72x72.png',
+    '/images/icons/icon-96x96.png',
+    '/images/icons/icon-128x128.png',
+    '/images/icons/icon-144x144.png',
+    '/images/icons/icon-152x152.png',
+    '/images/icons/icon-192x192.png',
+    '/images/icons/icon-384x384.png',
+    '/images/icons/icon-512x512.png'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request)
+                    .then(response => {
+                        if (!response || response.status !== 200 || response.type !== 'basic') {
+                            return response;
+                        }
+                        const responseToCache = response.clone();
+                        caches.open(CACHE_NAME)
+                            .then(cache => {
+                                cache.put(event.request, responseToCache);
+                            });
+                        return response;
+                    });
+            })
+    );
+}); 
